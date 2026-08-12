972daf5b2a0a3e091b41735b50eccccc96fa0ab0
darkh <yazidsaid5@gmail.com>
2026-08-12 00:33:37 -0500

    feat(js): agregar captura de credenciales en formulario de login

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//**Modulo de alternar entre formularios */
 const loginForm = document.getElementById('login-form');
 const registerForm = document.getElementById('register-form');
 const btnRegister = document.getElementById('btn-register');
@@ -11,6 +13,8 @@ function alternarFormularios() {
 btnRegister.addEventListener('click', alternarFormularios);
 btnLogin.addEventListener('click', alternarFormularios);
 
+//**Modulo de Validacion de la contraseña en el registro */
+
 const regPassword = document.getElementById('reg-password');
 const confPassword = document.getElementById('conf-password');
 
@@ -21,4 +25,14 @@ registerForm.addEventListener('submit',function(event){
     }else{
         alert("Las contraseñas no coinciden");
     }
-})
+});
+
+//**Modulo de captura de credenciales del inicio de sesion */
+
+const logEmail = document.getElementById('log-email');
+const logPassword = document.getElementById('log-password');
+
+loginForm.addEventListener('submit',function(event){
+    event.preventDefault();
+    alert(`INTENTANDO INICIAR SESION CON ${logEmail.value}`);
+});
